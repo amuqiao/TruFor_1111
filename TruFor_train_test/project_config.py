81ac677b713ddbbe7daba9b99cc154a5b9e44a69
wangluffy <978210873@qq.com>
2025-11-11 01:03:33 +0800

    初始化提交

---
+++ TruFor_train_test/project_config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+project_root = Path(__file__).parent
+
+dataset_paths = {
+    # Specify where are the roots of the datasets.
+    'IMD'      : 'dataset/data/IMD2020',
+    # 'FR'       : 'path/to/FR',
+    # 'CA'       : 'path/to/CA',
+    # 'tampCOCO' : 'path/to/tampCOCO',
+    # 'compRAISE': 'path/to/compRAISE',
+}
